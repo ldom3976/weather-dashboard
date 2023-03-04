718d4e2e99cef8ec0f510bff23cfe2ca638f37c4
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-03-04 11:00:58 -0500

    added to city function to fetch api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,55 @@ var cities = [];
  var cityInputEl = document.querySelector("#city");
  var searchButtonEl = document.querySelector("#search-btn");
  var weatherContainerEl = document.querySelector("#weather-container");
+ var currentWeatherEl = document.querySelector("#current-weather");
  var fiveDayContainerEl = document.querySelector("#five-day-container");
+ var headerContainerEl = document.querySelector("header-container");
+ var city = [];
 
- var apiKey = [];
+ var apiKey = "c9495534c42d51955c234e09ba782764";
 
  var formSubmit = function (event) {
-    var city = cityInputEl.ariaValueMax.trim();
+    //prevents page from refreshing
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
 
     if (city) {
         getCity(city);
-        getFiveDaye(city);
+        //getFiveDay(city);
 
         //clears old content
         cityInputEl.value = "";
     } else {
-        alert("Please enter a city");
+        alert("Please enter a city.");
     }
-    saveSearch();
+    saveSearch(city);
     pastSearch(city);
  }
 
  var getCity = function (city) {
     //fetch api
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+
+
+                });
+
+            } else {
+                alert("Error: " + response.statusText);
+                getCity();
+            }
+        })
+        .catch(function (error) {
+            alert("Cannot connect to OpenWeather");
+        });
  }
 
  var displayWeather = function () {
