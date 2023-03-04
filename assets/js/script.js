e20145dc8b972548ff6adbd5fb74e20b26ff0b66
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-03-04 10:29:04 -0500

    started initial variables in js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var cities = [];
+ var cityFormEl = document.querySelector("#city-form")
+ var cityInputEl = document.querySelector("#city");
+ var searchButtonEl = document.querySelector("#search-btn");
+ var weatherContainerEl = document.querySelector("#weather-container");
+ var fiveDayContainerEl = document.querySelector("#five-day-container");
+
+ var apiKey = [];
